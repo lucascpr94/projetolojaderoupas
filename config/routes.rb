e1050f011c3c436e48e849593073035428d361e6
Lucas Cardoso <lucascpr94@gmail.com>
2019-01-09 16:37:22 -0200

    Add new link to layouts app and new function destroy product id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/produtos/new" => "produtos#new"
+  get "/produtos/:id/remove" => "produtos#destroy"
   post "/produtos" => "produtos#create"
   root 'produtos#index'
 end
